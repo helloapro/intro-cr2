561817a8e97c7b9a0525f2bd748676c3fe81a92c
April Peng <helloaprilpeng@gmail.com>
2016-06-11 01:16:24 -0700

    add majority of content in html I want. complete and test full js functionality. not know how to make math more unique for city outcome - have to think on this

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,43 @@
 $(function() {
-  $(form#city-survey).submit(function(event) {
+  $("form#city-survey").submit(function(event) {
     event.preventDefault();
     var name = $("#name").val();
-    var protein = parseInt($("#protein")val());
-    var memory = parseInt($("#memory")val());
-    var alt-mind = parseInt($("#alt-mind")val());
-    var climate = parseInt($("#climate")val());
-    var disney = parseInt($("#disney")val());
-    var sum = protein+memory+alt-mind+climate+disney
+    var protein = parseInt($("#protein").val());
+    var memory = parseInt($("#memory").val());
+    var altmind = parseInt($("#altmind").val());
+    var climate = parseInt($("#climate").val());
+    var disney = parseInt($("#disney").val());
+    var sum = protein+memory+altmind+climate+disney
     var city;
+    var asap;
 
-    if ((sum/15) <= 1.5) {
+    if ((sum/5) <= 10.8) {
       city = "Cairo";
-    } else if ((sum/15) <= 2.5) {
+    } else if ((sum/5) <= 10.9) {
       city = "Chile";
-    } else if ((sum/15) <= 3.5) {
+    } else if ((sum/5) <= 20.9) {
       city = "Shanghai";
-    } else if ((sum/15) <= 4.5) {
+    } else if ((sum/5) <= 30.9) {
       city = "Berlin";
     } else {
       city = "Australia";
     }
 
-    $("#name").text(name);
-    $("#city").text(city);
+    if (city === "Cairo") {
+      asap = "في أقرب وقت ممكن";
+    } else if (city === "Chile") {
+      asap = "con rapidez";
+    } else if (city === "Berlin") {
+      asap = "mach schnell";
+    } else if (city === "Shanghai") {
+      asap = "尽快地";
+    } else {
+      asap = "flat chat";
+    }
+
+    $(".name").text(name);
+    $(".city").text(city);
+    $(".asap").text(asap);
     $("#city-result").show();
   });
 });
